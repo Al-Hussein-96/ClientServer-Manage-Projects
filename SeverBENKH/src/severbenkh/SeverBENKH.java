11b1df047a240dd7cf0ef8250651cc0e228d9b0e
mohamadkarttomeh <mohamadkarttomeh@gmail.com>
2018-03-11 12:53:56 -0700

    BufferReader add

--- SeverBENKH/src/severbenkh/SeverBENKH.java
+++ SeverBENKH/src/severbenkh/SeverBENKH.java
@@ -1,58 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package severbenkh;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SeverBENKH {
-    int Login_Port = 1;
-    int SiginUp_Port = 2;
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
 
-    public void LoginPort() throws IOException {
-        ServerSocket LoginPort = new ServerSocket(Login_Port);
+  
 
-        Socket ss = LoginPort.accept();
-        Scanner sc = new Scanner(ss.getInputStream());
-        
-        User NewUser = new User();
-        NewUser.Name = sc.nextLine();
-        NewUser.Password = sc.nextLine();
-        
-        
-        
-//        String temp;
-//        Scanner F = new Scanner(System.in);
-//        temp = F.nextLine();
-//        PrintStream p = new PrintStream(ss.getOutputStream());
-//        p.println(temp);
+    private static ServerSocket serverSocket;
+    private static final int PORT = 4321;
+
+    public static void main(String[] args) throws IOException {
+       try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (IOException ex) {
+            System.out.println("Unable to Connect to Port");
+            System.exit(1);
+        }
+
+        do {
+            Socket client = serverSocket.accept();
+            System.out.println("New Client Accepted");
+
+            ClientHandler handler = new ClientHandler(client);
+            handler.start();
+        } while (true);
 
     }
 
-    public void SiginUpPort() throws IOException {
-        ServerSocket SiginUpPort = new ServerSocket(SiginUp_Port);
-
-        Socket ss = SiginUpPort.accept();
-        Scanner sc = new Scanner(ss.getInputStream());
-        
-        User NewUser = new User();
-        NewUser.Name = sc.nextLine();
-        NewUser.Password = sc.nextLine();
-//        
-//        String temp;
-//        Scanner F = new Scanner(System.in);
-//        temp = F.nextLine();
-//        PrintStream p = new PrintStream(ss.getOutputStream());
-//        p.println(temp);
-
-    }
+    
 }
