849fadb27a9c66b2efec98d219bc7238bd3bdaa5
WasemBaerkdar <wasem.97.35@gmail.com>
2018-03-16 16:16:49 +0200

    3 windows

---
+++ Wasem/login/src/login/FXMLDocumentController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ /* To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login;
+
+import com.jfoenix.effects.JFXDepthManager;
+import com.sun.jndi.dns.DnsContextFactory;
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author Wasem
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    private Label label;
+    @FXML
+    private AnchorPane cardpanal;
+    
+ 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        JFXDepthManager.setDepth(cardpanal, 2);
+    }    
+
+   
+    
+}
