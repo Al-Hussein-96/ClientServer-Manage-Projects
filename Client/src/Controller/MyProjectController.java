034b1b9873fc03068a3470f5225b61eadc742ab3
Moaz <wolf97.m@hotmail.com>
2018-03-23 23:29:05 +0200

    Add button SignUp and some code in btnOpen

--- Client/src/Controller/MyProjectController.java
+++ Client/src/Controller/MyProjectController.java
@@ -32,19 +32,36 @@ import javafx.scene.control.TreeTableColumn;
 import javafx.util.Callback;
 
 public class MyProjectController implements Initializable {
-
+    
     @FXML
     private JFXTreeTableView<TabelProject> TabelView;
-
+    private List< CommonProject> MyProject;
+    
     @FXML
     void btnOpen(ActionEvent event) {
-
+        TreeItem<TabelProject> TI = TabelView.getSelectionModel().getSelectedItem();
+        CommonProject CP = null;
+        TabelProject TP = null;
+        if (TI != null) {
+            TP = TI.getValue();
+            System.out.println(TP.NameProject);
+        }
+        if (MyProject == null || TP == null) {
+            return;
+        }
+        for (int i = 0; i < MyProject.size(); i++) {
+            if (TP.equal(MyProject.get(i))) {
+                CP = MyProject.get(i);
+            }
+        }
+        if(CP==null)return;
+        System.out.println("Go");
+        //  HER GO TO THE NEXT WINDOW AND SENT CP TO SHOW IT 
     }
-
+    
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        List< CommonProject> MyProject = GetMyProject();
-
+        MyProject = GetMyProject();
         JFXTreeTableColumn<TabelProject, String> nameProject = new JFXTreeTableColumn<>("Name");
         nameProject.setPrefWidth(150);
         nameProject.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<TabelProject, String>, ObservableValue<String>>() {
@@ -53,7 +70,7 @@ public class MyProjectController implements Initializable {
                 return param.getValue().getValue().NameProject;
             }
         });
-
+        
         JFXTreeTableColumn<TabelProject, String> dataCreate = new JFXTreeTableColumn<>("Date");
         dataCreate.setPrefWidth(130);
         dataCreate.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<TabelProject, String>, ObservableValue<String>>() {
@@ -62,7 +79,7 @@ public class MyProjectController implements Initializable {
                 return param.getValue().getValue().DateCreate;
             }
         });
-
+        
         JFXTreeTableColumn<TabelProject, String> author = new JFXTreeTableColumn<>("Author");
         author.setPrefWidth(140);
         author.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<TabelProject, String>, ObservableValue<String>>() {
@@ -71,7 +88,7 @@ public class MyProjectController implements Initializable {
                 return param.getValue().getValue().Author;
             }
         });
-
+        
         JFXTreeTableColumn<TabelProject, String> numberOfContributors = new JFXTreeTableColumn<>("Contributors");
         numberOfContributors.setPrefWidth(140);
         numberOfContributors.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<TabelProject, String>, ObservableValue<String>>() {
@@ -80,7 +97,7 @@ public class MyProjectController implements Initializable {
                 return param.getValue().getValue().NumberOfContributors;
             }
         });
-
+        
         JFXTreeTableColumn<TabelProject, String> numberOfCommits = new JFXTreeTableColumn<>("Commits");
         numberOfCommits.setPrefWidth(130);
         numberOfCommits.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<TabelProject, String>, ObservableValue<String>>() {
@@ -90,7 +107,7 @@ public class MyProjectController implements Initializable {
             }
         });
         SimpleDateFormat ft = new SimpleDateFormat("yyyy.MM.dd 'at' HH:mm:ss");
-
+        
         ObservableList<TabelProject> users = FXCollections.observableArrayList();
         for (int i = 0; i < MyProject.size(); i++) {
             CommonProject CP = MyProject.get(i);
@@ -106,27 +123,27 @@ public class MyProjectController implements Initializable {
         TabelView.getColumns().setAll(nameProject, dataCreate, author, numberOfContributors, numberOfCommits);
         TabelView.setRoot(root);
         TabelView.setShowRoot(false);
-
+        
     }
-
+    
     private List<CommonProject> GetMyProject() {
         System.out.println("Send Request to Server for create List of myproject");
         networkOutput.println("MYPROJECT");
         List<CommonProject> mylist = null;
         try {
             String respone = networkInput.readLine();
-
+            
             if (respone.equals("Done")) {
 
                 /*
                 * here another Connect To Second ServerSocket For Send Files
                  */
                 Socket socket1 = new Socket(host, PORT1);
-
+                
                 DataInputStream dis = new DataInputStream(socket1.getInputStream());
-
+                
                 FileOutputStream fos = new FileOutputStream("temp.data");
-
+                
                 byte[] buffer = new byte[4096];
                 int filesize = dis.readInt(); // Send file size in separate msg
                 System.out.println("Size = " + filesize + "bytes");
@@ -140,14 +157,14 @@ public class MyProjectController implements Initializable {
                     System.out.println("read " + totalRead + " bytes.");
                     fos.write(buffer, 0, read);
                 }
-
+                
                 fos.close();
                 dis.close();
                 try {
                     mylist = (List<CommonProject>) ResourceManager.load("temp.data"); /// This List Must put inside TabelView
                     System.out.println("Number Of Project for User is : " + mylist.size());
                 } catch (Exception ex) {
-
+                    
                     System.err.println("Error : " + ex.getMessage());
                 }
             }
--- Client/src/Controller/PageMainController.java
+++ Client/src/Controller/PageMainController.java
@@ -4,11 +4,18 @@ import client.User;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class PageMainController implements Initializable {
 
@@ -35,6 +42,21 @@ public class PageMainController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    @FXML
+    private void SignOut(MouseEvent event) {
+        try {
+            roopane.getScene().getWindow().hide();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FXML/WindowsSelect.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.initStyle(StageStyle.TRANSPARENT);
+            stage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(WindowsSelectController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
