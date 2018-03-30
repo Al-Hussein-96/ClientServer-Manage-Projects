7e535a426015b05eff39f80ae1c3cc22d3b9ced0
Al-Hussein <m.alhussein.96@gmail.com>
2018-03-30 16:35:49 +0300

    we need fill browsers table

---
+++ Client/src/CommonClass/NameAndDirectory.java
@@ -0,0 +1,12 @@
+
+package CommonClass;
+
+public class NameAndDirectory {
+    public String Name;
+    public String Directory;
+    public NameAndDirectory(String Name , String Directory)
+    {
+        this.Name = Name;
+        this.Directory = Directory;
+    }
+}
